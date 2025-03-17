3f65750d6311d4710ce96a7911bab0c7c57cf92d
nahmanmate <192176108+nahmanmate@users.noreply.github.com>
2025-03-18 01:52:21 +1100

    Added non-functional buttons for calculator using javascript and array.

--- script.js
+++ script.js
@@ -1,3 +1,65 @@
+// UI
+
+// define data for calc layout
+const calculatorButtons = [
+    // Row 1
+    [
+        { id: "clear", text: "C" },
+        { id: "negative", text: "+/-" },
+        { id: "percentage", text: "%"},
+        { id: "divide", text: "÷"}
+    ],
+    // Row 2
+    [
+        { id: "seven", text: "7"},
+        { id: "eight", text: "8"},
+        { id: "nine", text: "9"},
+        {id: "multiply", text: "x"},
+    ],
+    // Row 3
+    [
+        { id: "four", text: "4" },
+        { id: "five", text: "5" },
+        { id: "six", text: "6"},
+        { id: "minus", text: "-"}
+    ],
+    // Row 4
+    [
+        { id: "one", text: "1" },
+        { id: "two", text: "2" },
+        { id: "three", text: "3"},
+        { id: "plus", text: "+"}
+    ],
+    // Row 5
+    [
+        { id: "zero", text: "0" },
+        { id: "decimal", text: "." },
+        { id: "equals", text: "equals"}
+    ]
+];
+
+function createButtons() {
+    const buttonsContainer = document.querySelector("#buttonsContainer");
+
+    calculatorButtons.forEach(row => {
+        const buttonRow = document.createElement("div");
+        buttonRow.setAttribute("class", "buttonRow");
+
+        row.forEach(button => {
+            const buttonElement = document.createElement("button");
+            buttonElement.setAttribute(`id`, `${button.id}` + `Button`)
+            buttonElement.textContent = button.text;
+            buttonRow.appendChild(buttonElement);
+        })
+        buttonsContainer.appendChild(buttonRow)
+    })
+}
+createButtons();
+// const calcUI = document.querySelector("#calculator");
+// create four rows of
+
+
+//// Calculator Math ////
 // Create variables for operator and values
 const a = 0;
 const b = 0;
@@ -49,9 +111,8 @@ const operate = function (a, b, operator) {
   }
   return answer
 };
-console.log(operate(2, 3, "invalid"))
 
 operate(a, b, operator); //TODO - remove or assign appropriately.
 
 // Export functions for testing
-export { addition, subtraction, multiplication, division, operate };
+// export { addition, subtraction, multiplication, division, operate };
